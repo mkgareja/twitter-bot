dfa95ad0729dcfd3f8a6be38a8976734a48fe73b
Mahesh Gareja <mahesh.gareja@etechtexas.com>
2018-10-18 18:54:15 +0530

    change

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const twitterApp = {
+var Twit = require('twit')
+var T = {
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token: process.env.ACCESS_TOKEN,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-    timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
+    timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+    strictSSL:true,
   };
   
   T.post('statuses/update', { status: 'hello world!1' }, function(err, data, response) {
