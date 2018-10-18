c3635774d1adf082f8d4e6de33d3d60bd789fe6c
Mahesh Gareja <mahesh.gareja@etechtexas.com>
2018-10-18 15:18:21 +0530

    init

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const twitterApp = {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  consumer_key:         'KPlm2eUAWG0IKkAVDzhKTmXKO',
+  consumer_secret:      'pKGETi7t49O9t59ymG8jtG5QZ3X3KWbJVlDzxFIlnsO6m0hEel',
+  access_token:         '1052180188459687936-3capospcbGaLmdXDYsqNgQIiSe9X69',
+  access_token_secret:  'gtFcyLSoYHJQAdVOe84UKeh7JL7sEkPSLSZ3IrSEdXAtz',
   timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
 };
 
