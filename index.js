21ba24fdeef6f98c9407b9fcbdc84068031c7187
Mahesh Gareja <mahesh.gareja@etechtexas.com>
2018-10-18 18:51:52 +0530

    change

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const AutoDM = require("./src/AutoDM.js");
-
-console.log("App started successfully 🙌🙌");
-
-AutoDM();
+const twitterApp = {
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
+  };
+  
+  T.post('statuses/update', { status: 'hello world!1' }, function(err, data, response) {
+    console.log(data)
+  })
+  
